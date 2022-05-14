7631e4a5bca9baf97572225684c8b5d707e52c8a
ArcticLeaf20 <99631130+ArcticLeaf20@users.noreply.github.com>
2022-05-13 17:26:01 -0700

    Add files via upload

---
+++ tempest.py
@@ -0,0 +1,12 @@
+def tempest_desc():
+    import time
+    print("In Verimestia there are 3 main classes each focusing on different attributes and skills!\n")
+    time.sleep(2)
+    print("Here are the 3 classes and their subsequent advantages/disadvantages\n")
+    time.sleep(2)
+    print("The Tempest:\nThe tempest was born in the slums of Veloburg in the eastern regions of Greater Verimestia, using the power of the natural forces the Tempest focuses on dealing massive damage and avoiding enemy attacks.\n")
+    time.sleep(2)
+    print("The Tempest starts with the following skills & equipment:\n")
+    time.sleep(1)
+    print("Armor: 0, Evasion: 10, Shield: 5.\nVitality: 0, Finesse: 10, Acuity: 5.\nDamage: 10, Damage Multiplier: 1.0, Weapon: Apprentice Wand\n")
+    time.sleep(2)
