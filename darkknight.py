57df7f41d4f2105835bb7e429020b39ffcc84135
ArcticLeaf20 <99631130+ArcticLeaf20@users.noreply.github.com>
2024-10-02 18:02:12 -0700

    Delete darkknight.py

--- darkknight.py
+++
@@ -1,8 +0,0 @@
-def darkknight_desc():
-    import time
-    print("The Dark Knight:\nInformation regarding the dark knight is not widely known, the information that is available\nnotes that the dark knight is a ruthless assassin supposedly created at the precipice of a ruthless war that ended hundreds of years ago. The Dark Knight focuses on dealing massive critical strikes and blocking enemy attacks.\n")
-    time.sleep(2)
-    print("The Dark Knight starts with the following skills & equipment:\n")
-    time.sleep(2)
-    print("Armor: 5, Evasion: 0, Shield: 10.\nVitality: 10, Finesse: 0, Acuity: 5.\nDamage: 5, Damage Multiplier: 1.3, Weapon: Apprentice Scimitar")
-    time.sleep(2)
