36f0fbab1b8a22e4ffae9751b25cb3c6d6f99bb0
ArcticLeaf20 <99631130+ArcticLeaf20@users.noreply.github.com>
2024-10-02 18:02:36 -0700

    Delete totemist.py

--- totemist.py
+++
@@ -1,8 +0,0 @@
-def totemist_desc():
-    import time
-    print("The Totemist:\nOriginating from the province of Glimborough in the great mountains of Verimestia, the Totemist specializes in harnessing the essence of powerful totems. The Totemist focuses on absorbing the damage of his enemies and healing himself.\n")
-    time.sleep(2)
-    print("The Totemist starts with the following skills & equipment:\n")
-    time.sleep(2)
-    print("Armor: 10, Evasion: 2, Shield: 3.\nVitality: 5, Finesse: 5, Acuity: 5.\nDamage: 7, Damage Multiplier: 0.8, Weapon: Apprentice Battle Axe")
-    time.sleep(2)
